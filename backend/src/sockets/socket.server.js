5b8c4c64297933ec60b1a19cbf469cdae61221a5
Sagardabhi816 <sagardabhi816@gmail.com>
2026-06-15 10:58:58 +0530

    Added system prompt & optimizations

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -6,7 +6,9 @@ const messageModel = require("../model/message.model");
 const { createMemory, queryMemory } = require("../services/vector.service");
 
 const cookie = require("cookie");
-const { QueryVectorFromJSON } = require("@pinecone-database/pinecone/dist/pinecone-generated-ts-fetch/db_data");
+const {
+  QueryVectorFromJSON,
+} = require("@pinecone-database/pinecone/dist/pinecone-generated-ts-fetch/db_data");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
@@ -31,81 +33,143 @@ function initSocketServer(httpServer) {
   });
 
   io.on("connection", (socket) => {
-
     socket.on("ai-message", async (messagePayload) => {
+      // Storing User Inputs In Local Database
+      // const message = await messageModel.create({
+      //   chat: messagePayload.chat,
+      //   user: socket.user._id,
+      //   content: messagePayload.content,
+      //   role: "user",
+      // });
 
-      // Storing user inputs in db
-     const message = await messageModel.create({
-        chat: messagePayload.chat,
-        user: socket.user._id,
-        content: messagePayload.content,
-        role: "user"
-      });
+      // const vectors = await aiService.generateVector(messagePayload.content);
 
-      const vectors = await aiService.generateVector(messagePayload.content)
+      // Runs code Simultaneously
+      // Storing User Inputs To Vector Database
+
+      const [message, vectors] = await Promise.all([
+        messageModel.create({
+          chat: messagePayload.chat,
+          user: socket.user._id,
+          content: messagePayload.content,
+          role: "user",
+        }),
+        aiService.generateVector(messagePayload.content),
+        
+      ]);
 
       await createMemory({
-        vectors,
-        messageId: message._id,
-        metadata:{
-          chat:messagePayload.chat,
-          user:socket.user._id,
-          text:messagePayload.content
+          vectors,
+          messageId: message._id,
+          metadata: {
+            chat: messagePayload.chat,
+            user: socket.user._id,
+            text: messagePayload.content,
+          },
+        })
 
-        }
-      })
-
+      /*
       const memory = await queryMemory({
-          queryVector:vectors,
-          limit:1,
-          metadata:{}
-      })
-
-      console.log("Memory is = ",memory)
+        queryVector: vectors,
+        limit: 3,
+        metadata: {},
+      });
 
       // Fetching Past chats
-      const chatHistory = (await messageModel .find({
+      const chatHistory = (
+        await messageModel
+          .find({
             chat: messagePayload.chat,
-          }).sort({ createdAt: -1 }).limit(20).lean()).reverse();
+          })
+          .sort({ createdAt: -1 })
+          .limit(20)
+          .lean()
+      ).reverse();
+*/
 
-      
+      const [memory, chatHistory] = await Promise.all([
+        queryMemory({
+          queryVector: vectors,
+          limit: 3,
+          metadata: {
+            user:socket.user._id},
+        }),
+        messageModel
+          .find({
+            chat: messagePayload.chat,
+          })
+          .sort({ createdAt: -1 })
+          .limit(20)
+          .lean()
+          .then((messages) => messages.reverse()),
+      ]);
+      const shortTermMemory = chatHistory.map((item) => {
+        return {
+          role: item.role,
+          parts: [{ text: item.content }],
+        };
+      });
+
+      const longTermMemory = [
+        {
+          role: "user",
+          parts: [
+            {
+              text: `These are some previous messages from the chat, use them to generate a response
+            
+            ${memory.map((item) => item.metadata.text).join("\n")}`,
+            },
+          ],
+        },
+      ];
 
       // Sending inputs without chathistoryy
       // const response = await aiService.generateResponse(messagePayload.content);
 
       // Sending inputs with chathistoryy
-      const response = await aiService.generateResponse(
-        chatHistory.map( item => {
-          return {
-            role: item.role,
-            parts: [{ text: item.content }],
-          }
-        }),
-      );
+      const response = await aiService.generateResponse([
+        ...longTermMemory,
+        ...shortTermMemory,
+      ]);
 
-      // Storing model response
-     const responseMessage =  await messageModel.create({
+
+       socket.emit("ai-response", {
+        content: response,
+        chat: messagePayload.chat,
+      });
+      /*
+      // Storing Model Response In Local Database
+      const responseMessage = await messageModel.create({
         chat: messagePayload.chat,
         user: socket.user._id,
         content: response,
         role: "model",
       });
 
-      const responseVectors = await aiService.generateVector(response)
-      await createMemory({
-        vectors:responseVectors,
-        messageId:responseMessage._id,
-        metadata:{
-          chat:messagePayload.chat,
-          user:socket.user._id,
-          text:response
-        }
-      })
+      const responseVectors = await aiService.generateVector(response);*/
 
-      socket.emit("ai-response", {
-        content: response,
-        chat: messagePayload.chat,
+      const [responseMessage, responseVectors] = await Promise.all([
+        messageModel.create({
+          chat: messagePayload.chat,
+          user: socket.user._id,
+          content: response,
+          role: "model",
+        }),
+        aiService.generateVector(response),
+      ]);
+
+      // Storing Agent Response To Vector Database
+      await createMemory({
+        vectors: responseVectors,
+        messageId: responseMessage._id,
+        metadata: {
+          chat: messagePayload.chat,
+          user: socket.user._id,
+          text: response,
+        },
       });
+
+     
     });
   });
 }
